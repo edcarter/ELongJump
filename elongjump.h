7b2b25c221c655096cf396c3d45db566942bf564
Elias Carter <edcarter@ualberta.ca>
2017-01-02 23:22:26 -0700

    Update elongjump.h

--- elongjump.h
+++ elongjump.h
@@ -6,8 +6,9 @@ struct jmp_buf {
 	long int sp;
 };
 
-// return
+// return 0 from initial call, and non-zero after longjump
 int esetjmp(struct jmp_buf * buf);
+
 void elongjmp(struct jmp_buf * buf);
 
 #endif
