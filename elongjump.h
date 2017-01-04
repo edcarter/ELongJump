70f3e8845f283bf23991aed9545c4b61d6e2dc6a
edcarter <edcarter@ualberta.ca>
2017-01-03 18:56:30 -0700

    add packed attribute to jmp_buf

--- elongjump.h
+++ elongjump.h
@@ -1,11 +1,11 @@
 #ifndef _ELONGJUMPH
 #define _ELONGJUMPH
 
-struct jmp_buf {
+struct __attribute__ ((__packed__))  jmp_buf {
 	long int pc;
 	long int sp;
 	long int bp; // base pointer
-};
+}; // pack struct to ensure safe access in assembly
 
 // return 0 from initial call, and non-zero after longjump
 int esetjmp(struct jmp_buf * buf);
